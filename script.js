5f3b3027a4f92840659ab2071d9fdc4ded0f4e11
Rishabh Patel <129976052+Rishabh2334@users.noreply.github.com>
2024-06-24 18:27:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Software Developer", "Content writer and Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
